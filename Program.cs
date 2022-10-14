740167ac6f47bca88f611236beb7f1ee72c0526b
Alexandr <jwchaganav@gmail.com>
2022-10-14 15:52:33 +0400

    определили ввод данных

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введете по порядку 3 числа, сначала первое: ");
+string? sNum1 = Console.ReadLine();
+
+Console.Write("Теперь второе: ");
+string? sNum2 = Console.ReadLine();
+
+Console.Write("Теперь третье: ");
+string? sNum3 = Console.ReadLine();
+
+int num1 = Convert.ToInt32(sNum1);
+int num2 = Convert.ToInt32(sNum2);
+int num3 = Convert.ToInt32(sNum3);
+
 Console.WriteLine("Hello, World!");
